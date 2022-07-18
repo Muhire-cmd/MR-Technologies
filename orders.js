59a64c807f01b7eb5ae836d0a41be89ae399cedd
muhire_cmd <muhirevickyson@gmail.com>
2022-07-18 18:34:11 +0200

    Final Design

---
+++ orders.js
@@ -0,0 +1,51 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'MAC book Air 13',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+
+    {
+        productName: 'iPad Air 3rd Generation',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'JBL Music Charger 4',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+
+    {
+        productName: 'Apple Watch i7',
+        productNumber: '22821',
+        paymentStatus: 'paid',
+        shipping: 'Delivered'
+    },
+
+    {
+        productName: 'TV stand',
+        productNumber: '78623',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'Apple Mouse',
+        productNumber: '46590',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+
+]
